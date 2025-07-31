e0714f7e9f810d9cf003df3a7c4034a84eee99fe
bluemoon983 <msdrobot@naver.com>
2025-07-31 01:04:24 -0700

    🔧 Add image

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const JSKApp());
+}
+
+class JSKApp extends StatelessWidget {
+  const JSKApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JSK App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(child: const Text('Welcome to JSK App!')),
+    );
+  }
+}
